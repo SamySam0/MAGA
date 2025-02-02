48b72041a48a23d34fa4b584177a92bfa627b863
Sam Belkadi <belkadisamuel@gmail.com>
2025-02-02 20:55:22 +0000

    Remake of the entire model architecture

--- model.py
+++ model.py
@@ -1,89 +1,103 @@
+''' 
+GraphRNN model: A hierarchical RNN, where the first (i.e., the graph-level) 
+RNN generates the nodes and maintains the state of the graph, while the second 
+(i.e., the edge-level) RNN generates the edges of a given node. 
+Implementation following details from https://arxiv.org/pdf/1802.08773 .
+'''
+
 import torch
+import torch.nn as nn
 import numpy as np
+from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
-class GraphRNN:
-    ''' 
-    GraphRNN model: A hierarchical RNN, where the first (i.e., the graph-level) 
-    RNN generates the nodes and maintains the state of the graph, while the second 
-    (i.e., the edge-level) RNN generates the edges of a given node. 
-    Implementation following details from https://arxiv.org/pdf/1802.08773 .
-    '''
-    def __init__(self, device, input_size=16, max_nodes=10):
 
-        self.MAX_NODES = max_nodes
+class GraphLevelRNN(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, num_layers, output_size):
+        super().__init__()
+        self.input_size = input_size
+        self.num_layers = num_layers
 
-        # Define tokens
-        self.SOS_TOKEN_GL = torch.tensor(
-            [3]*input_size, dtype=torch.float32,
-        ).unsqueeze(dim=0)
-
-        self.SOS_TOKEN_EL = torch.tensor(
-            [3], dtype=torch.float32
-        ).unsqueeze(dim=0)
-
-        # Graph-level RNN
-        self.graph_level_rnn = torch.nn.GRU(
-            input_size=input_size,
-            hidden_size=128,
-            num_layers=4,
+        self.linear_in = nn.Linear(
+            in_features=input_size,
+            out_features=embedding_size,
             dtype=torch.float32,
-            device=device,
         )
+        self.relu = nn.ReLU()
 
-        # Edge-level RNN
-        self.edge_input_dim_reduc = torch.nn.Linear(128, 16)
-
-        self.edge_level_rnn = torch.nn.GRU(
-            input_size=1,
-            hidden_size=16,
-            num_layers=4,
+        self.gru = nn.GRU(
+            input_size=embedding_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
             dtype=torch.float32,
-            device=device,
         )
 
-        self.edge_mlp = torch.nn.Sequential(
-            torch.nn.Linear(16, 8),
-            torch.nn.ReLU(),
-            torch.nn.Linear(8, 1),
-            torch.nn.Sigmoid(),
+        self.linear_out = nn.Linear(
+            in_features=hidden_size, 
+            out_features=output_size,
+            dtype=torch.float32,
         )
+
+        self.hidden = None # defaults to zeros tensor
+
+    def forward(self, x, x_lens):
+        x = torch.flatten(x, 2, 3) # TODO: this fixes a bug that shouldn't exist
+        x = self.relu(self.linear_in(x)) # [batch, seq_len, embedding_dim]
+        x = pack_padded_sequence(x, x_lens, batch_first=True, enforce_sorted=False)
+        x, self.hidden = self.gru(x, self.hidden) # [batch, seq_len, hidden_size]
+        # Unpack (reintroduce padding)
+        x, _ = pad_packed_sequence(x, batch_first=True)
+        x = self.linear_out(x) # [batch, seq_len, output_size]
+        return x
     
-    def forward(self):
-        edges = np.empty((self.MAX_NODES, self.MAX_NODES))
+    def reset_hidden(self):
+        self.hidden = None
 
-        hn_gl, x_gl = None, self.SOS_TOKEN_GL
-        for n in range(1, self.MAX_NODES):
 
-            # Graph-level RNN
-            _, hn_gl = self.graph_level_rnn(x_gl, hn_gl)
-            hn_el = self.edge_input_dim_reduc(hn_gl)
-            x_el = self.SOS_TOKEN_EL
+class EdgeLevelRNN(nn.Module):
+    def __init__(self, embedding_size, hidden_size, num_layers):
+        super().__init__()
+        self.num_layers = num_layers
 
-            # Edge-level RNN
-            for e in range(n):
-                out_el, hn_el = self.edge_level_rnn(x_el, hn_el) # TODO: Should we use GL output instead of GL hidden state to initialise EL (20:53)?
-                out_el = self.edge_mlp(out_el)
-                print(out_el.shape)
+        self.linear_in = nn.Linear(
+            in_features=1,
+            out_features=embedding_size,
+            dtype=torch.float32,
+        )
+        self.relu = nn.ReLU()
 
-                # Flip a coin given sigmoid probability
-                x_el = torch.bernoulli(out_el).item()
-                edges[n,e] = x_el
-                x_el = torch.tensor(
-                    [x_el], dtype=torch.float32
-                ).unsqueeze(dim=0)
-            
-            # Input to next cell of graph-level RNN is the hidden 
-            # state of the last edge-level RNN cell
-            x_gl = hn_el
+        self.gru = nn.GRU(
+            input_size=embedding_size,
+            hidden_size=hidden_size,
+            num_layers=num_layers,
+            batch_first=True,
+            dtype=torch.float32,
+        )
 
-            # Check for EOS (isolated node, no edges)
-            if sum(edges[n,:]) == 0:
-                break
-        
-        return edges
+        self.mlp_out = nn.Sequential(
+            nn.Linear(hidden_size, embedding_size),
+            nn.ReLU(),
+            nn.Linear(embedding_size, 1),
+            nn.Sigmoid(),
+        )
 
-if __name__ == "__main__":
-    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    model = GraphRNN(device=device)
-    edges = model.forward()
-    print(edges)
+        self.hidden = None # defaults to zeros tensor
+    
+    def set_first_layer_hidden(self, hn):
+        '''
+        Sets the hidden state of the first GRU layer to the output of the
+        graph-level RNN. The hidden state of all other layers are reset to 0.
+        '''
+        zeros = torch.zeros([self.num_layers-1, hn.shape[-2], hn.shape[-1]], device=hn.device)
+        if len(hn.shape) == 2:
+            hn = hn.unsqueeze(dim=0)
+        self.hidden = torch.cat([hn, zeros], dim=0) # [num_layers, batch_size, hidden_size]
+
+    def forward(self, x, x_lens):
+        x = self.relu(self.linear_in(x)) # [batch, seq_len, embedding_dim]
+        x = pack_padded_sequence(x, x_lens, batch_first=True, enforce_sorted=False)
+        x, self.hidden = self.gru(x, self.hidden) # [batch, seq_len, hidden_size]
+        # Unpack (reintroduce padding)
+        x, _ = pad_packed_sequence(x, batch_first=True)
+        x = self.mlp_out(x) # [batch, seq_len, 1 (sigmoid)]
+        return x
