6fc445dbad0cbbf125fbe9c7b6d07a318850bd6b
Sam Belkadi <belkadisamuel@gmail.com>
2025-02-27 22:36:27 +0000

    Fix CPU/GPU memory share error by passing tensor on common device

--- model/vgae_helpers.py
+++ model/vgae_helpers.py
@@ -12,7 +12,7 @@ def prepare_for_exp(annots_recon, adjs_recon, node_masks, edge_masks):
 
     adjs_recon = discretize(adjs_recon, masks=edge_masks)
     adjs_recon[:, :, :, 0] = adjs_recon[:, :, :, 0] + (1 - edge_masks.squeeze())
-    adjs_recon = torch.cat((adjs_recon[:, :, :, 1:], torch.zeros(adjs_recon.shape[0], adjs_recon.shape[1], adjs_recon.shape[2], 1)), dim=-1)
+    adjs_recon = torch.cat((adjs_recon[:, :, :, 1:], torch.zeros(adjs_recon.shape[0], adjs_recon.shape[1], adjs_recon.shape[2], 1, device=adjs_recon.device)), dim=-1)
     annots_recon = discretize(annots_recon, masks=masks)
     none_type = 1 - masks.float()
     annots_recon = torch.cat((annots_recon, none_type), dim=-1).detach().cpu()
