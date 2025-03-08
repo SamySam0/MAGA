6bd86cb47907c9b4af7b186ce50556d3ff43f860
Sam Belkadi <belkadisamuel@gmail.com>
2025-03-08 15:06:40 +0000

    Change gen_to_exp function to account for new kekulized format

--- model/vgae_helpers.py
+++ model/vgae_helpers.py
@@ -9,11 +9,14 @@ def prepare_for_exp(annots_recon, adjs_recon, node_masks):
     # Discretize adj and remove first feature ("no edge") since qm9_exp takes 4 features where all should be 0 when no edge
     edge_masks = get_edge_masks(node_masks)
     adjs_recon = discretize(adjs_recon, masks=edge_masks)       # Set to zero edges of masked nodes
-    adjs_recon = adjs_recon[:, :, :, 1:]                        # Set to zero edges with feature "no edge"
+    adjs_recon[:, :, :, 0] = adjs_recon[:, :, :, 0] + (1 - edge_masks.squeeze())
     adjs_recon = adjs_recon.permute(0, 3, 1, 2).detach().cpu()
 
     # Discretizer nodes
     annots_recon = discretize(annots_recon, masks=node_masks.unsqueeze(-1))   # Set to zero masked nodes (others are always nodes since interpolate to desired graph size already)
+    none_type = 1 - node_masks.unsqueeze(-1).float()
+    annots_recon = torch.cat((annots_recon, none_type), dim=-1).detach().cpu()
+
     return annots_recon, adjs_recon
 
 
